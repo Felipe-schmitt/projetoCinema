99b847860aa1bc5a70abc9de3627920678054f7a
João Vitor Miguez da Cunha <PET0147543@aluno.firjansenaisesi.com.br>
2023-09-18 08:56:37 -0300

    First commit

---
+++ projetocine/src/main/java/com/cinema/projetocine/ProjetocineApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.projetocine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetocineApplication.class, args);
+	}
+
+}
---
+++ projetocine/src/test/java/com/cinema/projetocine/ProjetocineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.projetocine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
